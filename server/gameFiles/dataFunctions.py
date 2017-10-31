d7abc717d8bb79307e2ab565fd8846e88872d9f7
root <root@group02.dhcp.nd.edu>
2017-10-31 16:17:24 -0400

    files

--- server/gameFiles/dataFunctions.py
+++ server/gameFiles/dataFunctions.py
@@ -1,30 +1,41 @@
-import psycopg2 
+import psycopg2, hashlib
 
+def dbInit():
+  conn = psycopg2.connect(database = 'fuzzytoads', user = 'fuzzytoad', password='databases', host = '127.0.0.1')
+  cur = conn.cursor()
+  return cur
 
 def test():
     conn = psycopg2.connect(database = 'fuzzytoads', user = 'fuzzytoad', password='databases', host = '127.0.0.1')
     cur = conn.cursor()
     for i in range(0,10):
-        cur.execute("INSERT into gamestate (hash, wp, turn, gamestate) values('test" + str(i) +"', 0, 0, 'xoxoxo')")
+        cur.execute("UPDATE gamestate set wp = " + str(i) + " where hash = 'test" + str(i) + "'")
+    
     conn.commit()
     conn.close()
 
 def queryBestAiMove(validMoves, token, board):
     win = 0. 
     bestMove = []
+    cur = dbInit()
+
+    #i = 8
     for cord in validMoves:
-        #tmpBoard = board
-        #tmpBoard[cord[0]][cord[1]] = token
-        #boardString = boardToString(tmpBoard)
-        for m in Move.objects.raw("SELECT * from gamestate where hash = %s", hashGamestate("test")):
-            print m
-'''            if m == null:
-                return false
-            if m > win:
-                win = m
-                bestMove = [cord[0], cord[1]]'''
-        
-    #return bestMove
+        tmpBoard = board
+        tmpBoard[cord[0]][cord[1]] = token
+        boardString = boardToString(tmpBoard)
+        cur.execute("SELECT * from gamestate where hash = '" + hashGamestate(boardString) + "'")
+        #cur.execute("SELECT * from gamestate where hash = 'test" +str(i) + "'")
+        row = cur.fetchall()
+        if len(row) is 0:
+          return False
+        for data in row:
+          if data[1] > win:
+              win = data[1]
+              bestMove = [cord[0], cord[1]]
+    
+    print bestMove
+    return bestMove
 
 def boardToString(board):
     tmpString = ""
@@ -34,5 +45,5 @@ def boardToString(board):
 
     return tmpString
 
-def hashGamestate(self, boardString):
-    return  hashlib.md5(board.encode()).hexdigest() 
+def hashGamestate(boardString):
+    return  hashlib.md5(boardString.encode()).hexdigest() 
