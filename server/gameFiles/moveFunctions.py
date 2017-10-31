1558f131cb93c79ae1c323882e7134072262e6e3
michaelburke1 <michaeljburke18@gmail.com>
2017-10-30 20:06:44 -0400

    files

--- server/gameFiles/moveFunctions.py
+++ server/gameFiles/moveFunctions.py
@@ -1,10 +1,22 @@
+import dataFunctions
+from random import randint
+
+
 def aiRandomMove(validMoves):
+    'Pick best of valid Moves'
     print "lol so randum"
-    return True
+    return validMoves[randint(0, len(validMoves))]
 
 def aiDatabaseMove(validMoves):
+    'Pick best of validMoves'
+    move = dataFunctions.queryBestAiMove(validMoves)
     print "wow such AI"
     return False
+
 def humanMove(row, col, token, board):
+    'Actually Perform Moves'
+    board[row][col] = token
     print "so hooman"
-    return False
+    'do database stuff (if any) here'
+    return board 
+
