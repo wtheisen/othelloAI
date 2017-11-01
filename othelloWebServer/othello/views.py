2025a947b5e6d2c594c1155bc84f72d292dad31d
djasek <djasek@group02.dhcp.nd.edu>
2017-10-31 21:10:11 -0400

    getting basic new page running

--- othelloWebServer/othello/views.py
+++ othelloWebServer/othello/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
 
 def index(request):
-        return HttpResponse("Hello, world. You're at the polls index.")
+    #template = loader.get_template('othello/index.html')
+    return render(request, 'othello/index.html')
