0f1963baa3cf22c188a74e0b5f3ce75fe41d0b68
djasek <djasek@group02.dhcp.nd.edu>
2017-10-31 22:52:03 -0400

    got post request to work

--- othelloWebServer/othello/urls.py
+++ othelloWebServer/othello/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$othello/index2.html', views.index2, name='index'),
+    url(r'^test_post$', views.test_post, name='test_post'),
 ]
