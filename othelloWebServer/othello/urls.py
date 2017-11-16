5b7886ac30efdb8dc9d96b989fb8121fc0fac941
jbadilla <jbadilla@group02.dhcp.nd.edu>
2017-11-16 15:05:33 -0500

    Connected Board to Django -- Still require session variables to work in views.py

--- othelloWebServer/othello/urls.py
+++ othelloWebServer/othello/urls.py
@@ -3,7 +3,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$othello/index2.html', views.index2, name='index'),
+    url(r'^index2$', views.index2, name='index'),
     url(r'^test_post$', views.test_post, name='test_post'),
     url(r'^$', views.main, name='main'),
+    url(r'^update$', views.new_move, name='new_move')
 ]
--- othelloWebServer/othello/views.py
+++ othelloWebServer/othello/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from django.views.decorators.csrf import csrf_exempt
+import json
 
 import sys
 sys.path.append('/home/djasek/othelloAI/othelloWebServer/othello/gameFiles')
@@ -28,3 +29,46 @@ def test_post(request):
         request.session['num'] = num
         print "num: ", num
         return render(request, 'othello/index.html')
+
+@csrf_exempt
+def new_move(request):
+
+	# NOTE: Assume P1 is 'O' (Black) and AI is 'X' (White)
+
+	response = {}
+
+	# Player 1 makes a new_movee
+	if request.method == 'POST':
+		row = request.POST['row']
+		column = request.POST['column']
+
+		if playerMove(row, column, "O"):
+
+			response['end'] = 'false'
+
+			if gameEnd():
+				response['end'] = 'true'
+
+			response['gamestate'] = Object.gamestate
+			response['score'] = str(getScore("O")) + " : " + str(getScore("X"))
+			response['result'] = 'success'
+
+		else:
+			response['result'] = 'failure'
+			response['message'] = 'Invalid move!'
+
+	# AI makes a move
+	elif request.method == 'GET':
+
+		aiMove()
+
+		response['end'] = 'false'
+		
+		if gameEnd():
+			response['end'] = 'true'
+
+		response['gamestate'] = Object.gamestate
+		response['score'] = str(getScore("O")) + " : " + str(getScore("X"))
+		response['result'] = 'success'
+
+	return JsonResponse(response)
