c1292da066db8a4ff1aee525d0db650da0642b60
djasek <djasek@group02.dhcp.nd.edu>
2017-11-17 14:58:10 -0500

    trying to work on adding static files

--- othelloWebServer/othelloWebServer/urls.py
+++ othelloWebServer/othelloWebServer/urls.py
@@ -3,6 +3,9 @@ from django.conf.urls import *
 from django.contrib import admin
 admin.autodiscover()
 
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+import settings
+
 urlpatterns = [
     # Examples:
     # url(r'^$', 'othelloWebServer.views.home', name='home'),
@@ -10,3 +13,6 @@ urlpatterns = [
     url(r'^othello/', include('othello.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
+
+if settings.DEBUG == True:
+    urlpatterns += staticfiles_urlpatterns()
