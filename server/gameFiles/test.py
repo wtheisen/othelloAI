56917f1c3a1d4d702c51f5d523cfb78f302969c6
mburke18 <mburke18@group02.dhcp.nd.edu>
2017-12-02 13:46:38 -0500

    deleted

--- server/gameFiles/test.py
+++
@@ -1,6 +0,0 @@
-import dataFunctions, gameFunctions
-
-
-#dataFunctions.test()
-moves = [[1,1], [2,2], [3,3], [4,4], [5,5], [6,6], [7,7], [8,8], [9,9]]
-dataFunctions.queryBestAiMove(moves, 'X', "test")
